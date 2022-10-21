140a73aeac2870fa11436a69468f0398b52e3a86
sol <ocosio0907@gmail.com>
2022-10-21 20:13:07 +0900

    import react-native-url-polyfill/auto

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import "react-native-url-polyfill/auto";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
