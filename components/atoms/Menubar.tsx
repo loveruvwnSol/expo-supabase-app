08ba7c20128e77055a1381244a660078e84ce7f5
sol <ocosio0907@gmail.com>
2023-01-26 12:27:37 +0900

    deleted

---
+++ components/atoms/Menubar.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useRef } from "react";
+import {
+  Box,
+  Menu,
+  Pressable,
+  HamburgerIcon,
+  Center,
+  AlertDialog,
+  Button,
+} from "native-base";
+
+export const Menubar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClose = () => setIsOpen(false);
+
+  const cancelRef = useRef(null);
+  return (
+    <Center>
+      <Button colorScheme="danger" onPress={() => setIsOpen(!isOpen)}>
+        Delete Customer
+      </Button>
+      <AlertDialog
+        leastDestructiveRef={cancelRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <AlertDialog.Content>
+          <AlertDialog.CloseButton />
+          <AlertDialog.Header>Delete Customer</AlertDialog.Header>
+          <AlertDialog.Body>
+            This will remove all data relating to Alex. This action cannot be
+            reversed. Deleted data can not be recovered.
+          </AlertDialog.Body>
+          <AlertDialog.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="unstyled"
+                colorScheme="coolGray"
+                onPress={onClose}
+                ref={cancelRef}
+              >
+                Cancel
+              </Button>
+              <Button colorScheme="danger" onPress={onClose}>
+                Delete
+              </Button>
+            </Button.Group>
+          </AlertDialog.Footer>
+        </AlertDialog.Content>
+      </AlertDialog>
+    </Center>
+  );
+};
