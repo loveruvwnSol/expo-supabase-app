230f2f96bae90916956b6bbccbfc3974c1fe54c9
sol <ocosio0907@gmail.com>
2023-02-17 20:19:59 +0900

    enabled UI change,split fucntion and  selfIntro change

--- components/organisms/UserNameSettings.tsx
+++ components/organisms/UserNameSettings.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
-import { Box, Text, Button, VStack, Input, useColorMode } from "native-base";
+import {
+  Box,
+  Text,
+  VStack,
+  Input,
+  useColorMode,
+  IconButton,
+  TextArea,
+  Divider,
+  ScrollView,
+} from "native-base";
 import { supabase } from "../../libs/supabaseClient";
 import { Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 type UserNameSettingsProps = {
   navigation: any;
@@ -12,16 +23,34 @@ export const UserNameSettings: React.FC<UserNameSettingsProps> = ({
 }) => {
   const [username, setUsername] = useState("");
   const [userid, setUserid] = useState("");
+  const [selfIntro, setSeflIntro] = useState("");
   const [loading, setLoading] = useState(false);
   const { colorMode } = useColorMode();
 
-  function updateData() {
+  function updateName() {
+    if (!username || !username.match(/\S/g)) return;
     supabase
       .from("profiles")
       .update([
         {
           id: supabase.auth.user()?.id,
           user_name: username,
+        },
+      ])
+      .then(({ error }) => {
+        if (error) Alert.alert(error.message);
+        setLoading(false);
+        if (!error) navigation.navigate("UserInfo");
+      });
+  }
+
+  function updateId() {
+    if (!userid || !userid.match(/\S/g)) return;
+    supabase
+      .from("profiles")
+      .update([
+        {
+          id: supabase.auth.user()?.id,
           user_id: userid,
         },
       ])
@@ -32,45 +61,99 @@ export const UserNameSettings: React.FC<UserNameSettingsProps> = ({
       });
   }
 
+  function updateSelfIntro() {
+    if (!selfIntro || !selfIntro.match(/\S/g)) return;
+    supabase
+      .from("profiles")
+      .update([
+        {
+          id: supabase.auth.user()?.id,
+          selfIntro: selfIntro,
+        },
+      ])
+      .then(({ error }) => {
+        if (error) Alert.alert(error.message);
+        setLoading(false);
+        if (!error) navigation.navigate("UserInfo");
+      });
+  }
+
   return (
-    <VStack alignItems="center">
-      <Box mb={5}>
-        <Text fontWeight="thin" mb={2}>
-          ニックネーム
-        </Text>
-        <Input
-          bg={colorMode === "dark" ? "coolGray.700" : "blueGray.100"}
-          placeholderTextColor={colorMode === "dark" ? "white" : "black"}
-          w={72}
-          fontWeight="thin"
-          mb={5}
-          variant="outline"
-          placeholder="ニックネーム"
-          onChangeText={(text) => setUsername(text)}
-          value={username}
-        />
-        <Text fontWeight="thin" mb={2}>
-          ユーザーID
-        </Text>
-        <Input
-          bg={colorMode === "dark" ? "coolGray.700" : "blueGray.100"}
-          placeholderTextColor={colorMode === "dark" ? "white" : "black"}
-          w={72}
-          fontWeight="thin"
-          variant="outline"
-          placeholder="ユーザーID"
-          onChangeText={(text) => setUserid(text)}
-          value={userid}
-        />
-      </Box>
-      <Button
-        w={"48"}
-        borderRadius={0}
-        disabled={loading}
-        onPress={() => updateData()}
-      >
-        設定する
-      </Button>
-    </VStack>
+    <ScrollView>
+      <VStack alignItems="center">
+        <Box mb={5}>
+          <Text fontWeight="thin" mt={2} mb={2}>
+            ニックネーム
+          </Text>
+          <Input
+            bg={colorMode === "dark" ? "coolGray.700" : "blueGray.100"}
+            placeholderTextColor={colorMode === "dark" ? "white" : "black"}
+            w={72}
+            fontWeight="thin"
+            mb={5}
+            variant="outline"
+            placeholder="ニックネーム"
+            onChangeText={(text) => setUsername(text)}
+            value={username}
+            InputRightElement={
+              <IconButton
+                opacity={username ? 1 : 0.5}
+                bg={"green.400"}
+                icon={<Ionicons name="checkmark" color="green" />}
+                disabled={loading}
+                onPress={() => updateName()}
+              />
+            }
+          />
+          <Text fontWeight="thin" mt={2} mb={2}>
+            ユーザーID
+          </Text>
+          <Input
+            bg={colorMode === "dark" ? "coolGray.700" : "blueGray.100"}
+            placeholderTextColor={colorMode === "dark" ? "white" : "black"}
+            w={72}
+            fontWeight="thin"
+            mb={5}
+            variant="outline"
+            placeholder="ユーザーID"
+            onChangeText={(text) => setUserid(text)}
+            value={userid}
+            InputRightElement={
+              <IconButton
+                opacity={userid ? 1 : 0.5}
+                bg={"green.400"}
+                icon={<Ionicons name="checkmark" color="green" />}
+                disabled={loading}
+                onPress={() => updateId()}
+              />
+            }
+          />
+          <Text fontWeight="thin" mt={2} mb={2}>
+            ひとこと
+          </Text>
+          <TextArea
+            variant="unstyled"
+            bg={colorMode === "dark" ? "coolGray.700" : "blueGray.100"}
+            placeholderTextColor={colorMode === "dark" ? "white" : "black"}
+            w={72}
+            fontWeight="thin"
+            placeholder="ひとこと"
+            onChangeText={(text) => setSeflIntro(text)}
+            value={selfIntro}
+            autoCompleteType={undefined}
+            InputRightElement={
+              <IconButton
+                opacity={selfIntro ? 1 : 0.5}
+                bg={"green.400"}
+                icon={<Ionicons name="checkmark" color="green" />}
+                disabled={loading}
+                onPress={() => updateSelfIntro()}
+              />
+            }
+          />
+          <Divider w={72} />
+        </Box>
+      </VStack>
+    </ScrollView>
   );
 };
