dbabdd5cd5c6fc1d907815161631a8a974b692df
sol <ocosio0907@gmail.com>
2023-02-24 19:47:40 +0900

    add image upload and update when add post

--- components/organisms/AddPostText.tsx
+++ components/organisms/AddPostText.tsx
@@ -7,11 +7,15 @@ import {
   TextArea,
   Button,
   useColorMode,
+  Image,
+  IconButton,
 } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
 import { supabase } from "../../libs/supabaseClient";
 import { Alert } from "react-native";
-import { usePost } from "../../hooks/useTlinfo";
+import { usePost, usePostStore } from "../../hooks/useTlinfo";
+import * as ImagePicker from "expo-image-picker";
+import { nanoid } from "../../libs/nanoid";
 
 type AddPostText = {
   user: any;
@@ -21,10 +25,36 @@ type AddPostText = {
 export const AddPostText: React.FC<AddPostText> = ({ user, onSendFinish }) => {
   const { colorMode } = useColorMode();
   const [textAreaValue, setTextAreaValue] = useState("");
+  const [image, setImage] = useState<string | undefined>();
+  const [formData, setFormData] = useState<string | FormData>("");
   const { getPostInfo } = usePost();
+  const { update } = usePostStore();
+
   const AddPostData = async () => {
-    if (!textAreaValue || !textAreaValue.match(/\S/g)) return;
-    if (textAreaValue.length >= 1) {
+    if (!textAreaValue.match(/\S/g) && image) {
+      var result = nanoid();
+      console.log(result);
+      supabase
+        .from("timeline")
+        .insert({
+          id: user && user.id,
+          text: textAreaValue,
+          post_image_id: result,
+        })
+        .then(({ error }) => {
+          if (error) Alert.alert(error.message);
+          Alert.alert("投稿が完了しました");
+          setTextAreaValue("");
+          getPostInfo();
+          onSendFinish();
+        });
+
+      const { error } = await supabase.storage
+        .from("post-images")
+        .upload(user.id + "/" + result, formData, { upsert: false });
+      console.log(error);
+    }
+    if (textAreaValue.length >= 1 && !image) {
       supabase
         .from("timeline")
         .insert({
@@ -38,11 +68,70 @@ export const AddPostText: React.FC<AddPostText> = ({ user, onSendFinish }) => {
           getPostInfo();
           onSendFinish();
         });
+    } else if (textAreaValue.length >= 1 && image) {
+      var result = nanoid();
+      console.log(result);
+      supabase
+        .from("timeline")
+        .insert({
+          id: user && user.id,
+          post_image_id: result,
+        })
+        .then(({ error }) => {
+          if (error) Alert.alert(error.message);
+          Alert.alert("投稿が完了しました");
+          setTextAreaValue("");
+          getPostInfo();
+          onSendFinish();
+        });
+
+      const { error } = await supabase.storage
+        .from("post-images")
+        .upload(user.id + "/" + result, formData, { upsert: false });
+
+      console.log(error);
+    }
+    update();
+  };
+
+  const uploadFromURI = async (photo: ImagePicker.ImagePickerResult) => {
+    if (!photo.cancelled) {
+      const ext = photo.uri.substring(photo.uri.lastIndexOf(".") + 1);
+
+      const fileName = photo.uri.replace(/^.*[\\\/]/, "");
+
+      var formData = new FormData();
+      formData.append("files", {
+        // @ts-ignore-next-line
+        uri: photo.uri,
+        name: fileName,
+        type: photo.type ? `image/${ext}` : `video/${ext}`,
+      });
+
+      setImage(photo.uri);
+      setFormData(formData);
+    } else {
+      return photo;
+    }
+  };
+
+  const pickImage = async () => {
+    let photo = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    try {
+      return await uploadFromURI(photo);
+    } catch (e) {
+      return null;
     }
   };
 
   return (
-    <Box>
+    <Box bg={colorMode === "dark" ? "coolGray.900" : "blueGray.100"}>
       <HStack alignItems="center" justifyContent="space-between" w={72}>
         <Text ml={2} fontSize={16}>
           {user && user.user_name}
@@ -62,9 +151,32 @@ export const AddPostText: React.FC<AddPostText> = ({ user, onSendFinish }) => {
           autoCompleteType={undefined}
         />
       </Box>
+      <Box justifyContent="center" alignItems="center">
+        {image && (
+          <Box>
+            <Box position="absolute">
+              <IconButton
+                top={24}
+                icon={<Ionicons name="close-circle-outline" size={24} />}
+              />
+            </Box>
+            <Image
+              mb={3}
+              w={72}
+              h={72}
+              source={{ uri: image }}
+              alt="postImage"
+              borderRadius={15}
+            />
+          </Box>
+        )}
+      </Box>
       <Divider />
       <HStack mt={2} alignItems="center" justifyContent="space-between">
-        <Ionicons name="image-outline" size={20} color="dodgerblue" />
+        <IconButton
+          icon={<Ionicons name="image-outline" size={20} color="dodgerblue" />}
+          onPress={() => pickImage()}
+        />
         <Button
           onPress={AddPostData}
           w={24}
