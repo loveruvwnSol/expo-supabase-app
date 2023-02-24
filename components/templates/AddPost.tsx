f3b69e52ef6493f3dce149cd192341ab71482936
sol <ocosio0907@gmail.com>
2023-02-24 19:48:29 +0900

    add ScrollView

--- components/templates/AddPost.tsx
+++ components/templates/AddPost.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, HStack, useColorMode, Avatar, Button } from "native-base";
+import { Box, HStack, useColorMode, Avatar, ScrollView } from "native-base";
 import { useUserIcon, useUserInfo } from "../../hooks/useUserInfo";
 import { AddPostText } from "../organisms/AddPostText";
 import { useNavigation } from "@react-navigation/native";
@@ -11,32 +11,34 @@ export const AddPost = () => {
   const nav = useNavigation();
 
   return (
-    <Box
-      bg={colorMode === "dark" ? "coolGray.900" : "blueGray.100"}
-      w="full"
-      h="full"
-      alignItems="center"
-    >
-      <Box mt={10} w={96} justifyContent="space-between">
-        <Box m={4}>
-          <HStack alignItems="center" justifyContent="space-between">
-            <HStack>
-              <Box mt={1}>
-                <Avatar
-                  w={12}
-                  h={12}
-                  borderWidth={0.5}
-                  borderColor="gray.500"
-                  source={{ uri: usericon }}
-                  size="xs"
-                  ml={1}
-                />
-              </Box>
-              <AddPostText user={user} onSendFinish={() => nav.goBack()} />
+    <ScrollView>
+      <Box
+        bg={colorMode === "dark" ? "coolGray.900" : "blueGray.100"}
+        w="full"
+        h="full"
+        alignItems="center"
+      >
+        <Box mt={10} w={96} justifyContent="space-between">
+          <Box m={4}>
+            <HStack alignItems="center" justifyContent="space-between">
+              <HStack>
+                <Box mt={1}>
+                  <Avatar
+                    w={12}
+                    h={12}
+                    borderWidth={0.5}
+                    borderColor="gray.500"
+                    source={{ uri: usericon }}
+                    size="xs"
+                    ml={1}
+                  />
+                </Box>
+                <AddPostText user={user} onSendFinish={() => nav.goBack()} />
+              </HStack>
             </HStack>
-          </HStack>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </ScrollView>
   );
 };
