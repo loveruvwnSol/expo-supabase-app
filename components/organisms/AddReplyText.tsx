9fd9dad102eaec209cd04a3b3b05526961481c26
loveruvwnSol <ocosio0907@gmail.com>
2023-02-25 23:46:30 +0900

    add reply system with image

--- components/organisms/AddReplyText.tsx
+++ components/organisms/AddReplyText.tsx
@@ -7,11 +7,15 @@ import {
   TextArea,
   Button,
   useColorMode,
+  IconButton,
+  Image,
 } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
 import { supabase } from "../../libs/supabaseClient";
 import { Alert } from "react-native";
 import { usePostStore, useReply } from "../../hooks/useTlinfo";
+import * as ImagePicker from "expo-image-picker";
+import { nanoid } from "../../libs/nanoid";
 
 type AddReplyTextProps = {
   user: any;
@@ -27,11 +31,36 @@ export const AddReplyText: React.FC<AddReplyTextProps> = ({
 }) => {
   const { colorMode } = useColorMode();
   const [textAreaValue, setTextAreaValue] = useState("");
+  const [image, setImage] = useState<string | undefined>();
+  const [formData, setFormData] = useState<string | FormData>("");
   const { getReplyInfo } = useReply(post_id);
   const { update } = usePostStore();
-  const AddPostData = async () => {
-    if (!textAreaValue || !textAreaValue.match(/\S/g)) return;
-    if (textAreaValue.length >= 1) {
+
+  const AddReplyData = async () => {
+    if (!textAreaValue.match(/\S/g) && image) {
+      var result = nanoid();
+      supabase
+        .from("replies")
+        .insert({
+          id: user && user.id,
+          text: textAreaValue,
+          post_id: post_id,
+          reply_image_id: result,
+        })
+        .then(({ error }) => {
+          if (error) Alert.alert(error.message);
+          Alert.alert("投稿が完了しました");
+          setTextAreaValue("");
+          getReplyInfo();
+          onSendFinish();
+        });
+
+      const { error } = await supabase.storage
+        .from("post-images")
+        .upload(user.id + "/" + result, formData, { upsert: false });
+      console.log(error);
+    }
+    if (textAreaValue.length >= 1 && !image) {
       supabase
         .from("replies")
         .insert({
@@ -41,17 +70,76 @@ export const AddReplyText: React.FC<AddReplyTextProps> = ({
         })
         .then(({ error }) => {
           if (error) Alert.alert(error.message);
-          Alert.alert("返信が完了しました");
+          Alert.alert("投稿が完了しました");
           setTextAreaValue("");
           getReplyInfo();
           onSendFinish();
         });
+    } else if (textAreaValue.length >= 1 && image) {
+      var result = nanoid();
+      supabase
+        .from("replies")
+        .insert({
+          id: user && user.id,
+          text: textAreaValue,
+          post_id: post_id,
+          reply_image_id: result,
+        })
+        .then(({ error }) => {
+          if (error) Alert.alert(error.message);
+          Alert.alert("投稿が完了しました");
+          setTextAreaValue("");
+          getReplyInfo();
+          onSendFinish();
+        });
+
+      const { error } = await supabase.storage
+        .from("post-images")
+        .upload(user.id + "/" + result, formData, { upsert: false });
+
+      console.log(error);
     }
     update();
   };
 
+  const uploadFromURI = async (photo: ImagePicker.ImagePickerResult) => {
+    if (!photo.cancelled) {
+      const ext = photo.uri.substring(photo.uri.lastIndexOf(".") + 1);
+
+      const fileName = photo.uri.replace(/^.*[\\\/]/, "");
+
+      var formData = new FormData();
+      formData.append("files", {
+        // @ts-ignore-next-line
+        uri: photo.uri,
+        name: fileName,
+        type: photo.type ? `image/${ext}` : `video/${ext}`,
+      });
+
+      setImage(photo.uri);
+      setFormData(formData);
+    } else {
+      return photo;
+    }
+  };
+
+  const pickImage = async () => {
+    let photo = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    try {
+      return await uploadFromURI(photo);
+    } catch (e) {
+      return null;
+    }
+  };
+
   return (
-    <Box>
+    <Box bg={colorMode === "dark" ? "coolGray.900" : "blueGray.100"}>
       <HStack alignItems="center" justifyContent="space-between" w={72}>
         <Text ml={2} fontSize={16}>
           {user && user.user_name}
@@ -71,11 +159,30 @@ export const AddReplyText: React.FC<AddReplyTextProps> = ({
           autoCompleteType={undefined}
         />
       </Box>
+      {image && (
+        <Box alignItems="left">
+          <IconButton
+            onPress={() => setImage(undefined)}
+            icon={<Ionicons name="close-circle-outline" size={24} />}
+          />
+          <Image
+            mb={3}
+            w={72}
+            h={72}
+            source={{ uri: image }}
+            alt="postImage"
+            borderRadius={15}
+          />
+        </Box>
+      )}
       <Divider />
       <HStack mt={2} alignItems="center" justifyContent="space-between">
-        <Ionicons name="image-outline" size={20} color="dodgerblue" />
+        <IconButton
+          icon={<Ionicons name="image-outline" size={20} color="dodgerblue" />}
+          onPress={() => pickImage()}
+        />
         <Button
-          onPress={AddPostData}
+          onPress={AddReplyData}
           w={24}
           h={10}
           bg={colorMode === "dark" ? "coolGray.900" : "blueGray.100"}
