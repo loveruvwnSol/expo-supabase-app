b33d547ed4fb777e1efc180f56fc6c60b81cb409
Shogo Arai <59103922+loveruvwnSol@users.noreply.github.com>
2024-07-01 10:24:06 +0900

    Update supabaseClient.ts

--- libs/supabaseClient.ts
+++ libs/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vnydgstacjbighveoveb.supabase.co" || "";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZueWRnc3RhY2piaWdodmVvdmViIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ1OTg1MDEsImV4cCI6MTk4MDE3NDUwMX0.nZPkXcECxu7qWDAw19ZeoTLoeo8r_NO682g2IRWH6ZA" || "";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string;
 export const supabase = createClient(supabaseUrl, supabaseKey);
